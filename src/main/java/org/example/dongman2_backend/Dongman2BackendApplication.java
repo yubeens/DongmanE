7786a70b84c6ece70bfea43a6018dc73a4f60a8c
DESKTOP-R4K628H\it <realisshobeen@gmail.com>
2025-07-24 11:34:03 +0900

    구성도,erd파일 업로드

---
+++ src/main/java/org/example/dongman2_backend/Dongman2BackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.dongman2_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dongman2BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dongman2BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/dongman2_backend/Dongman2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dongman2_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dongman2BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
